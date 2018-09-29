f63bd56922f97fc8f54318eb8e4681e50a174fa1
root <root@localhost.localdomain>
2018-09-29 13:08:17 -0400

    Add Options Interface

--- App/modules/DroneModuleFrame.py
+++ App/modules/DroneModuleFrame.py
@@ -1,7 +1,19 @@
 
 class DroneModuleFrame(object):
-    def __init__(self, name):
+    def __init__(self, name, options):
         self.name = name
+        self.info = "Help"
+        self.options = options
+
+    def get_options(self):
+        print('-'*60)
+        print('| Name | Value | Description |')
+        for key, value in self.options.items():
+            print('| {} | {} | {} |'.format(key, value['value'], value['description']))
+        print('-'*60)
+
+    def set_option(self, name, value):
+        self.options[name]['value'] = value
 
     def Analyze(self):
         raise NotImplementedError()
--- Hack.py
+++ Hack.py
@@ -4,3 +4,6 @@ import sys
 i = interface.DroneInterface("example")
 i.Exploit()
 i.Analyze()
+i.get_options()
+i.set_option('example', 'new value')
+i.get_options()
