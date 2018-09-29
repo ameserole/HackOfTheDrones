0cb1de8c841a0e43edf443d25327ea284fe7cff7
Ryan Vrecenar <ryanvrecenar@gmail.com>
2018-09-29 13:24:56 -0500

    update CLI for executing analyze and exploit

--- App/hackdrones.py
+++ App/hackdrones.py
@@ -1,5 +1,10 @@
-from .shell import start_menu
+import sys
+sys.path.insert(0, '.')
+from shell import start_menu
 
 def main():
     start_menu()
     print("exiting")
+
+if __name__ == "__main__":
+    main() 
