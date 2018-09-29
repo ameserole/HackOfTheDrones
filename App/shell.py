a3e5c2e74b43e2f2a36b574af876f08535668bbd
Ryan Vrecenar <ryanvrecenar@gmail.com>
2018-09-29 13:51:38 -0500

    fixed prompt for more assistance

--- App/shell.py
+++ App/shell.py
@@ -12,14 +12,18 @@ def list_drone_wifi():
     print("Press any key to continue...")
     sys.stdin.read(1)
 
-def list_modules():
+def check_modules():
     modules_dir = os.path.abspath(os.path.dirname(__file__))
     modules = os.listdir(modules_dir + "/modules")
     invalid_modules = []
     for module in modules:
-        if not os.path.isfile("modules/%s/__init__.py"%module):
+        if not os.path.isfile("App/modules/%s/__init__.py"%module):
             invalid_modules.append(module)
     modules = list(set(modules) - set(invalid_modules))
+    return modules
+
+def list_modules():
+    modules = check_modules()
     print("Modules available for loading...")
     print()
     for module in modules:
@@ -33,12 +37,30 @@ def print_list_usage():
     return
 
 def print_run_usage():
-    return
+    print("Available Commands:")
+    print("\t\tanalyze - Executes Analyze function of DroneModule")
+    print("\t\texploit - Executes Exploit function of DroneModule")
+    print("\t\thelp - Displays this menu help")
+
+def print_module_usage():
+    print("Available Commands:")
+    print("\t\tset <name> <value> - Set <name> field of module with <value>")
+    print("\t\tshow - Shows fields of module")
+    print("\t\thelp - Displays this menu help")
+
+def print_menu_usage():
+    print("Available Commands:")
+    print("\t\tlist - Display enumeration of arguement")
+    print("\t\tuse - Loads module from modules directory")
+    print("\t\tmodule - Access and Manipulate loaded module")
+    print("\t\trun - Execute module behavior of argument")
+    print("\t\tshow - Display this menu help")
 
 def start_menu():
     module_loaded = False
     loaded_module = ""
     drone_inf = ""
+    valid_modules = check_modules()
     while True:
         module_prompt = " exploit(%s)"%(colored(loaded_module, 'red'))
         cmd = input("hackdrones%s>>>"%(module_prompt if module_loaded else ""))
@@ -46,26 +68,38 @@ def start_menu():
         if arguments[0] == "list":
             if arguments[1] == "modules":
                 list_modules()
-                pass
             elif arguments[1] == "wifi":
                 list_drone_wifi()
-                pass
             else:
                 print_list_usage()
         elif arguments[0] == "use":
-            loaded_module = arguments[1]
-            module_loaded = True
-            drone_inf = DroneInterface(loaded_module)
+            if arguments[1] in valid_modules:
+                loaded_module = arguments[1]
+                module_loaded = True
+                drone_inf = DroneInterface(loaded_module)
+            else:
+                print("Module was not a valid DroneModule")
+        elif arguments[0] == "module":
+            if arguments[1] == "set":
+                drone_inf.set_option(arguments[2], arguments[3])
+            elif arguments[1] == "show":
+                drone_inf.get_options()
+            elif arguments[1] == "help":
+                print_module_usage()
+            else:
+                print_module_usage()
         elif arguments[0] == "run":
             if arguments[1] == "analyze":
                 drone_inf.Analyze()
-                pass
             elif arguments[1] == "exploit":
                 drone_inf.Exploit()
-                pass
+            elif arguments[1] == "help":
+                print_run_usage()
             else:
                 print_run_usage()
+        elif arguments[0] == "show":
+            print_menu_usage()
         elif arguments[0] == "quit":
             break
         else:
-            print_usage()
+            print_menu_usage()
