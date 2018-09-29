4157be7db6ed5b09821c7d2797fb09006ddda41f
DrPadfoot <bryceolinger16@gmail.com>
2018-09-29 16:11:22 -0500

    Added base files for kismet_config & arpspoof dir

---
+++ App/modules/kismet_config/drone_lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+import socket
+import request
+
+lookup = [DJI, Parrot, Yuneec, Kespry, Autel, Autel Robotics, Insitu, EHang, Aeryon Labs, CyPhy, senseFly, Aerialtronics, Freefly, Flyability, Draganfly Innovations, Action Drone USA, GoPro, Intel, Ambarella, FLIR, Workswell, UDI, Hubsan, Cheerson, Syma Toys, Sky Viper, Blade, AguaDrone, Embention, Force1, Gryphon Sensors, Fat Shark, BFD Systems, Atlas Dynamics, SicDrone, TuffWing, Airspace, Aptonomy, SkySpecs, Dedrone, SparrowHawk, Flytrex, Aerialtronics, JJRC, Syma, UDI RC Toys, UDI, Walkera, Cheerson Hobby, Power Vision]
+
