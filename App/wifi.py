24b63d17c2fa73263fe38b0ab864313866b89ab6
DrPadfoot <bryceolinger16@gmail.com>
2018-09-29 15:10:34 -0500

    Enumerates wifi APs in area, and connects.

--- App/wifi.py
+++ App/wifi.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # wifi_discovery.py asks for a network interface, then utilizes that for discovering/rediscovering networks in the area. Once a network's BSSID is selected, it will attack the network with default credentials obtained from elsewhere. Python3 and NetworkManager/nmcli required.
-
+# Try to disconnect from networks before you run. Sometimes gives weird output of only network
 
 
 import subprocess, re
 
 
 # Detect network card/list interfaces and disconnect if needed
-netiface = input("List interface: ")
+netiface = input('List interface: ')
 print(netiface)
 yn = subprocess.Popen(['sudo','nmcli','c'],stdout=subprocess.PIPE)
 ynout = yn.stdout.read().decode('utf-8')
@@ -17,22 +17,16 @@ if netiface in ynout:
 
 # list all interfaces and choose which you want
 subprocess.run(['sudo', 'nmcli', 'd', 'wifi', 'rescan'])
-netlist = subprocess.Popen(['sudo', 'nmcli', 'd', 'wifi'],stdout=subprocess.PIPE)
+netlist = subprocess.Popen(['sudo', 'nmcli', 'd', 'wifi'], stdout=subprocess.PIPE)
 output = netlist.stdout.read().decode('utf-8')
 print(output)
-bssid = input('Input bssid: ')
+ssid = input('Input ssid: ')
 
 
-# Connect to drone using default creds
-# import "default_creds" from elsewhere, user/pass delimited by single space
+# Connect to drone using creds you found
+password = input('password: ')
 
-"""
+subprocess.run(['sudo','nmcli','d','wifi','connect',ssid,'password',password,'ifname',netiface])
 
-username pass
-username pass
-username pass
+print("Success.")
 
-"""
-
-for line in open('r',default_creds.txt):
-    
